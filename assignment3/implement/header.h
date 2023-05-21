ce8e8d65ea79f7e9b01bbd1929e24a8c49618a64
dongtimes2 <dongtimes2@gmail.com>
2023-05-21 14:33:14 +0900

    implement 예제코드 추가

---
+++ assignment3/implement/header.h
@@ -0,0 +1,4 @@
+#include <iostream>
+using namespace std;
+
+#define TEMP_NUMBER 100
---
+++ assignment3/implement/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "header.h"
+
+using namespace std;
+
+
+int main(void) {
+  cout << "hello world" << endl;
+  cout << TEMP_NUMBER << endl;
+
+  return 0;
+}
