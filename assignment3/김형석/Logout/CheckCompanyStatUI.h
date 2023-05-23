60d4fa2eed63183d57b8a377b6c95ec47704d00f
KHS1002 <132794218+KHS1002@users.noreply.github.com>
2023-05-23 15:04:03 +0900

    Add files via upload

---
+++ assignment3/김형석/Logout/CheckCompanyStatUI.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include "header.h"
+
+class CompanyMember;
+class Application;
+class Recruitment;
+class CompanyStat;
+
+class CheckCompanyStatUI {
+public:
+    void startInterface(CompanyMember* );
+};
